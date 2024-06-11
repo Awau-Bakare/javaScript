6ff8c3cea9196857c789b7729f7a01e6dd4953ee
Awau-Bakare <awaubakare@gmail.com>
2024-06-11 22:20:56 +0100

    update

--- index.js
+++ index.js
@@ -1,1 +1,107 @@
 console.log('Hello World')
+
+//variable and value
+//we can variable is a container that holds value
+// for variable declaration in javascript, there are three keywords
+
+//var, let, const
+// var firstName;
+
+// var firstName = "Hawau";
+// // console.log(firstName);
+
+// let x = 6;
+// // console.log(x);
+
+// const y = 5;
+// console.log(x % y);y
+
+//blocked scope
+
+// var x = 5;
+// var x = 6;
+// var x = 7;
+
+// let x = 5;
+// let x = 6;
+
+// const x = 5;
+// const x = 6;
+
+// console.log(x);
+
+// ways of declaring variables in javascript
+
+//camelcase, snakecase, using dollar sign, underscore
+
+const _last = "Shola";
+const lastName = "Shade";
+const last_name = "Titi";
+const $last = "Bisi";
+
+//dont
+// const var = 'Shola'
+// const super = 'me'
+// const 2women = 'Female'
+
+Variable declaration keywords 
+
+Variable name a
+And value
+
+
+string is text ..once it is inside a quotation mark eg "shade"
+Or "5"
+
+Const.log(typeof text)Number is number without a quotation mark 
+
+Intija number without decimal 5
+
+Float is number with decimal 0.5
+
+Console.log(
+    Boolean true or false 
+
+Const isAged = false
+Const hasGraduated =true
+
+Console.log(typeof hasGraduated)
+
+Undefined is data is said to be undefined when the data is not declared or not assigned a value
+Eg 
+Let x;
+
+Console.log(typeof x)
+Console.log(x)
+
+Null 
+
+Const hisName = null 
+Console.log(typeof hisName)
+
+bigInt when data is having a big number value 
+Const bigData = BigInt("123456789")
+Console.log(typeof bigData)
+
+Non primitive it is provided by data structure 
+Which means that the 2 data types can contain all the data types 
+
+Array allows the use of angular bracket 
+
+Const arrayNumber = [1, 2, 3, 4, 6, 7, 8, 9];
+
+Const stringArray = [ "Apple", "Mango", "Banana"]
+
+This mean array can contain the 6 data types above
+
+Object is also a key and value 
+
+Can also allow the other 6data types 
+
+Const person = {
+"name": "shade",
+age: 18,
+country: Nigeria 
+}
+
+Console.log(person)
